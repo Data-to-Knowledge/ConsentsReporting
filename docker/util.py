deaeb3200ee75dd65877c6b4bf346d16a79b32c8
Mike Kittridge <mullenkamp1@gmail.com>
2020-01-10 14:13:47 +1300

    added docker option

--- docker/util.py
+++ docker/util.py
@@ -12,13 +12,13 @@ from datetime import datetime
 ### Misc functions
 
 ## Log
-def log(server, database, table, run_time_start, data_from_time, internal_table, run_result, comment):
+def log(server, database, table, run_time_start, data_from_time, internal_table, run_result, comment, username=None, password=None):
     """
 
     """
     run_time_end = datetime.today().strftime('%Y-%m-%d %H:%M:%S')
     log1 = pd.DataFrame([[run_time_start, run_time_end, data_from_time, internal_table, run_result, comment]], columns=['RunTimeStart', 'RunTimeEnd', 'DataFromTime', 'InternalTable', 'RunResult', 'Comment'])
-    mssql.to_mssql(log1, server, database, table)
+    mssql.to_mssql(log1, server, database, table, username=username, password=password)
 
     return log1
 
