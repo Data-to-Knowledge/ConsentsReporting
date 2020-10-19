4da36136a02353e646cfb2b2bd615fcfb2743bb5
Mike Kittridge <mullenkamp1@gmail.com>
2020-10-20 09:03:17 +1300

    fix for the ts lowflow crc

--- reporting_tables_ts.py
+++ reporting_tables_ts.py
@@ -58,14 +58,14 @@ try:
     ## Determine last restriction date run
 
     stmt2 = max_date_stmt.format(table=schema1 + '.' + table1)
-    last_date1 = pd.Timestamp(mssql.rd_sql(param['output']['server'], param['output']['database'], stmt=stmt2, username=param['output']['username'], password=param['output']['password']).loc[0][0])
+    last_date1 = mssql.rd_sql(param['output']['server'], param['output']['database'], stmt=stmt2, username=param['output']['username'], password=param['output']['password']).loc[0][0]
 
     if last_date1 is None:
         stmt3 = min_date_stmt.format(table=lowflow_ts_table)
         last_date1 = pd.Timestamp(mssql.rd_sql(param['output']['server'], param['output']['database'], stmt=stmt3, username=param['output']['username'], password=param['output']['password']).loc[0][0])
-        last_date2 = last_date1
+        last_date2 = pd.Timestamp(last_date1)
     else:
-        last_date2 = last_date1 + pd.Timedelta(days=1)
+        last_date2 = pd.Timestamp(last_date1) + pd.Timedelta(days=1)
 
     print('Last sucessful date is ' + str(last_date1), ' New data to query will be ' + str(last_date2))
 
@@ -96,14 +96,14 @@ try:
     ## Determine last restriction date run
 
     stmt2 = max_date_stmt.format(table=schema1 + '.' + table1)
-    last_date1 = pd.Timestamp(mssql.rd_sql(param['output']['server'], param['output']['database'], stmt=stmt2, username=param['output']['username'], password=param['output']['password']).loc[0][0])
+    last_date1 = mssql.rd_sql(param['output']['server'], param['output']['database'], stmt=stmt2, username=param['output']['username'], password=param['output']['password']).loc[0][0]
 
     if last_date1 is None:
         stmt3 = min_date_stmt.format(table=lowflow_ts_table)
         last_date1 = pd.Timestamp(mssql.rd_sql(param['output']['server'], param['output']['database'], stmt=stmt3, username=param['output']['username'], password=param['output']['password']).loc[0][0])
-        last_date2 = last_date1
+        last_date2 = pd.Timestamp(last_date1)
     else:
-        last_date2 = last_date1 + pd.Timedelta(days=1)
+        last_date2 = pd.Timestamp(last_date1) + pd.Timedelta(days=1)
 
     print('Last sucessful date is ' + str(last_date1), ' New data to query will be ' + str(last_date2))
 
